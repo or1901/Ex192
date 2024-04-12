ad5db8771e0e33e022a2b130d967f00e86017c84
Ori Roitzaid <oriroitzaid@gmail.com>
2024-04-12 19:20:58 +0300

    Added helper class which saves the pointers to the roots in the database

---
+++ app/src/main/java/com/example/ex192/FBRef.java
@@ -0,0 +1,17 @@
+package com.example.ex192;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * FBRef class:
+ * an helper class for the DB - saves the pointers of the firebase DB.
+ * @author Ori Roitzaid <or1901 @ bs.amalnet.k12.il>
+ * @version	1
+ * @since 12/4/2024
+ */
+public class FBRef {
+    public static FirebaseDatabase FBDB = FirebaseDatabase.getInstance();
+
+    public static DatabaseReference refStudents = FBDB.getReference("Students");
+}
