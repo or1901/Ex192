31dd47e2176eeb21740d56f3f07e21be94a86533
Ori Roitzaid <oriroitzaid@gmail.com>
2024-05-03 17:42:29 +0300

    Connected the grades spinner with the grades array, and adjusted a little the design

--- app/src/main/java/com/example/ex192/AddStudentActivity.java
+++ app/src/main/java/com/example/ex192/AddStudentActivity.java
@@ -1,14 +1,32 @@
 package com.example.ex192;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddStudentActivity extends AppCompatActivity {
+    String[] grades = {"6th", "7th", "8th", "9th", "10th", "11th", "12th"};
+    Spinner spGrades;
+    ArrayAdapter<String> spinnerAdp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+
+        initViews();
+    }
+
+    /**
+     * This function initializes the views objects.
+     */
+    private void initViews() {
+        spGrades = findViewById(R.id.spGrades);
+
+        spinnerAdp = new ArrayAdapter<>(this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, grades);
+        spGrades.setAdapter(spinnerAdp);
     }
 }
