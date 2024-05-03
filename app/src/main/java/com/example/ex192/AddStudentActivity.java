6e72ba0cd51dbcfff7065e6ef62c5fe033af3c7b
Ori Roitzaid <oriroitzaid@gmail.com>
2024-05-03 17:07:15 +0300

    Added the design of the AddStudentActivity

---
+++ app/src/main/java/com/example/ex192/AddStudentActivity.java
@@ -0,0 +1,14 @@
+package com.example.ex192;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_student);
+    }
+}
