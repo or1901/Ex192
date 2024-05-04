5fe62a795bf6649984a5dd5cfbb0abec256e8287
Ori Roitzaid <oriroitzaid@gmail.com>
2024-05-04 18:21:51 +0300

    Made the final design of the date edit text, and coded its functionality - it saves the picked date

--- app/src/main/java/com/example/ex192/AddStudentActivity.java
+++ app/src/main/java/com/example/ex192/AddStudentActivity.java
@@ -1,14 +1,22 @@
 package com.example.ex192;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ex192.Objects.Vaccine;
+
+import java.util.Calendar;
 
 public class AddStudentActivity extends AppCompatActivity {
     String[] grades = {"6th", "7th", "8th", "9th", "10th", "11th", "12th"};
@@ -16,6 +24,9 @@ public class AddStudentActivity extends AppCompatActivity {
     ArrayAdapter<String> spinnerAdp;
     AlertDialog.Builder adb;
     LinearLayout vaccineDialog;
+    EditText dialogEtPlace, dialogEtDate;
+    int currentVaccine;
+    Vaccine[] vaccinesData;
 
     DialogInterface.OnClickListener onDialogBtnClick = new DialogInterface.OnClickListener() {
         @Override
@@ -49,10 +60,18 @@ public class AddStudentActivity extends AppCompatActivity {
         spinnerAdp = new ArrayAdapter<>(this,
                 androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, grades);
         spGrades.setAdapter(spinnerAdp);
+
+        currentVaccine = 0;
+        vaccinesData = new Vaccine[2];
+        vaccinesData[0] = new Vaccine();
+        vaccinesData[1] = new Vaccine();
     }
 
     private void displayVaccineDialog(int vaccineNum) {
         vaccineDialog = (LinearLayout) getLayoutInflater().inflate(R.layout.vaccine_dialog, null);
+
+        dialogEtPlace = vaccineDialog.findViewById(R.id.dialogEtPlace);
+        dialogEtDate = vaccineDialog.findViewById(R.id.dialogEtDate);
 
         adb = new AlertDialog.Builder(this);
 
@@ -67,10 +86,35 @@ public class AddStudentActivity extends AppCompatActivity {
     }
 
     public void getFirstVaccineData(View view) {
+        currentVaccine = 0;
         displayVaccineDialog(1);
     }
 
     public void getSecondVaccineData(View view) {
+        currentVaccine = 1;
         displayVaccineDialog(2);
+    }
+
+    public void dialogChooseDate(View view) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                AddStudentActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        dialogEtDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                        calendar.set(year, monthOfYear + 1, dayOfMonth);
+
+                        vaccinesData[currentVaccine].setDate(calendar);
+                    }
+                },
+                year, month, day);
+
+        datePickerDialog.show();
     }
 }
